72af9a11d68016fa59e43714dfada0abeab2a61d
afzalbek97 <129912802+afzalbek97@users.noreply.github.com>
2023-05-15 23:11:15 +0900

    Add files via upload

---
+++ Python_PROJECT_01.py
@@ -0,0 +1,18 @@
+#unit 3 심사문
+print('Hello, world!') 
+print('Hello, world!')
+
+#unit 5 심사문제
+print(102 *0.6+225)
+
+#unit 6_1 심사문제
+a=50
+b=100
+c=None
+
+#unit 6_2 심사문제
+kor, eng, math, scien = map(int, input().split()) 
+print((kor+eng+math+scien)//4)
+
+#unit 7 심사문제
+print(year, m
